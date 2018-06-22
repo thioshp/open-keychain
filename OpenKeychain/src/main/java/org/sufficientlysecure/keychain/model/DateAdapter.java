fdfd1d931a9ccc0463e9c14e9a7bebe693a093f3
Vincent Breitmoser <look@my.amazin.horse>
2018-06-22 19:23:00 +0200

    extract subkey loading from KeychainProvider

--- OpenKeychain/src/main/java/org/sufficientlysecure/keychain/model/DateAdapter.java
+++ OpenKeychain/src/main/java/org/sufficientlysecure/keychain/model/DateAdapter.java
@@ -7,6 +7,7 @@ import android.support.annotation.NonNull;
 
 import com.squareup.sqldelight.ColumnAdapter;
 import org.sufficientlysecure.keychain.model.AutocryptPeer.GossipOrigin;
+import org.sufficientlysecure.keychain.pgp.CanonicalizedSecretKey.SecretKeyType;
 
 
 final class CustomColumnAdapters {
@@ -49,4 +50,16 @@ final class CustomColumnAdapters {
         }
     };
 
+    public static final ColumnAdapter<SecretKeyType,Long> SECRET_KEY_TYPE_ADAPTER = new ColumnAdapter<SecretKeyType, Long>() {
+        @NonNull
+        @Override
+        public SecretKeyType decode(Long databaseValue) {
+            return databaseValue == null ? SecretKeyType.UNAVAILABLE : SecretKeyType.fromNum(databaseValue.intValue());
+        }
+
+        @Override
+        public Long encode(@NonNull SecretKeyType value) {
+            return (long) value.getNum();
+        }
+    };
 }
